d526cb7af62ed2109c1fd5dc1f57ad17f423436e
Wilmer Gonzalez <wagc.19889@gmail.com>
2021-08-08 10:05:30 -0500

    Adding docker support

---
+++ ChatMe.Api/Controllers/Health/HealthController.cs
@@ -0,0 +1,24 @@
+﻿namespace ChatMe.Api.Controllers.Health
+{
+    using ChatMe.Api.Controllers.Shared;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/ping")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Ping()
+        {
+            string now = DateTime.Now.ToLongDateString();
+
+            return Ok($"Pong at - {now}");
+        }
+    }
+}
